8d6321d289b72fe2648f48f8236143ec6d8c73f1
Mik00000 <mikjarkov@gmail.com>
2024-11-18 12:37:46 +0200

    Feature: Header Start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { Header } from './components/Header.tsx';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export const Header = () => {
+  const logo = require("../assets/logo.png");
+  const menuImg = require("../assets/menu.svg");
+
   return (
     <header className="header">
-        <img src="" alt="" />
+      <img src={logo} alt="logo" className="logo"/>
+      <nav className="nav-list">
+        <ul>
+          <li>Home</li>
+          <li>Product</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <div className="login-and-menu">
+        <button className="login-btn btn">Login</button>
+        <button className="menu-btn btn"><img src={menuImg} alt="menu" /></button>
+      </div>
     </header>
-  )
-}
+  );
+};
