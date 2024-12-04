988fc1ba1c8c3054111412981eb54b9b32964fbc
Mik00000 <troutundefined5894@gmail.com>
2024-12-04 21:35:49 +0200

    Feature: Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Features } from './components/Features.tsx';
 import { Header } from './components/Header.tsx';
+import { Hero } from './components/Hero.tsx';
 import './styles/App.css';
-
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Header/>
+    <Router>
+      <Header />
+    </Router>
+      <Hero/>
+      <Features/>
     </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="heading">
+        <h1>Work at the speed of thought</h1>
+        <h2>
+          Most calendars are designed for teams. Slate is designed for
+          freelancers who want a simple way to plan their schedule.
+        </h2>
+      </div>
+      <div className="buttons">
+        <button className="try-btn">Try For Free</button>
+        <button className="learn-btn">Learn More</button>
+      </div>
+      <div className="banner-background"></div>
+    </section>
+  );
+};
