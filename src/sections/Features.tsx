8c66d35fe002a52aedcc902d8477bf30a9dab672
Mik00000 <mikjarkov@gmail.com>
2024-12-05 19:47:02 +0200

    Feature: Features Block

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,25 +1,30 @@
 import React from "react";
+import feature1 from "../assets/banner/features-1.png";
+import feature2 from "../assets/banner/features-2.png";
 
 export const Features = () => {
   const benefits = [
     {
+      id:0,
       heading: "A single source of truth",
       text: "When you add work to your Slate calendar we automatically calculate useful insights ",
       icon: "benefits1.svg",
     },
     {
+      id:1,
       heading: "Intuitive interface",
       text: "When you add work to your Slate calendar we automatically calculate useful insights ",
       icon: "benefits2.svg",
     },
     {
+      id:2,
       heading: "Or with rules",
       text: "When you add work to your Slate calendar we automatically calculate useful insights ",
       icon: "benefits3.svg",
     },
   ];
   return (
-    <div>
+    <section className="features">
       <div className="heading">
         <h1>FEATURES</h1>
         <h2>
@@ -28,11 +33,14 @@ export const Features = () => {
         </h2>
       </div>
       <div className="info-block">
-        <div className="left-part"></div>
+        <div className="left-part">
+          <img src={feature1} alt="feature-banner" />
+          <img src={feature2} alt="feature-banner" />
+        </div>
         <div className="right-part">
           <ul>
             {benefits.map((block) => (
-              <li>
+              <li key={block.id}>
                 <div className="heading">
                 <img src={require(`../assets/icons/${block.icon}`)} alt="benefit-icon" />
                 <h3>{block.heading}</h3>
@@ -43,6 +51,6 @@ export const Features = () => {
           </ul>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
